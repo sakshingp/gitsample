071a4875bf6929bfa61f3f5f6065639aaf7286c0
sakshingp <sakshi.nagpal12@gmail.com>
2018-09-11 11:15:24 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,3 +1,3 @@
 var version = 6;
 // importScripts('https://cdn.pushcrewstaging.com/sw/a86e5cec678f816f8615fb48dac667b8.js');
-importScripts('https://cdn.pushcrewstaging.com/sw/ea5abe144843a5b5b5823d2eaae21c3e.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/8e9b9277a4ffeb8e7304c3bc74933df5.js');
