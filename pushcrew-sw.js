f4976da33588c66d3b0da60c2e2172e8484f4490
sakshingp <sakshi.nagpal12@gmail.com>
2018-11-22 13:33:29 +0530

    Add files via upload

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,3 +1,2 @@
 var version = 6;
-// importScripts('https://cdn.pushcrewstaging.com/sw/a86e5cec678f816f8615fb48dac667b8.js');
-importScripts('https://cdn.pushcrewstaging.com/sw/2a3ec47278f20c3134d7689687962270.js');
+importScripts('https://cdn.pushcrew.com/sw/2466c4a7541ec1c5da7dace27808c54d.js');
